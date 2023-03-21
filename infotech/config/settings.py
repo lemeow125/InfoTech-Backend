7a7c9229639463ef32d85eb8688d70d917dd1941
keannu125 <noehbernasol0@gmail.com>
2023-03-21 20:54:44 +0800

    Rename main app to config

--- infotech/config/settings.py
+++ infotech/config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'infotech.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'infotech.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
