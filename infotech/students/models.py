23a4a4a190ae6cd75a07bcf3b4255965118c3405
keannu125 <noehbernasol0@gmail.com>
2023-04-22 13:05:14 +0800

    Polished choice fields for students

--- infotech/students/models.py
+++ infotech/students/models.py
@@ -5,14 +5,14 @@ from django.db import models
 class Student(models.Model):
 
     class YearLevels(models.TextChoices):
-        FIRST_YEAR = 'IU-Y1', '1st Year'
-        SECOND_YEAR = 'IU-Y2', '2nd Year'
-        THIRD_YEAR = 'IU-Y3', '3rd Year'
+        FIRST_YEAR = '1st Year'
+        SECOND_YEAR = '2nd Year'
+        THIRD_YEAR = '3rd Year'
         FOURTH_YEAR = 'IU-Y4', '4th Year'
 
     class Semesters(models.TextChoices):
-        FIRST_SEM = 'Sem-1', '1st Semester'
-        SECOND_SEM = 'Sem-2', '2nd Semester'
+        FIRST_SEM = '1st Semester'
+        SECOND_SEM = '2nd Semester'
 
     class SexChoices(models.TextChoices):
         MALE = 'M', 'Male'
