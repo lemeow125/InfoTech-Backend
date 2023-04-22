44a81dc6fe92fc7103f737611c66ca629d22029c
keannu125 <noehbernasol0@gmail.com>
2023-04-22 16:08:13 +0800

    Remove extra viewsets from subjects

--- infotech/subjects/views.py
+++ infotech/subjects/views.py
@@ -8,27 +8,3 @@ class SubjectViewSet(viewsets.ModelViewSet):
     # permission_classes = [IsAuthenticated]
     serializer_class = SubjectSerializer
     queryset = Subject.objects.all().order_by('-year_level')
-
-
-class FirstYearSubjectViewSet(generics.ListAPIView):
-    # permission_classes = [IsAuthenticated]
-    serializer_class = SubjectSerializer
-    queryset = Subject.objects.filter(year_level='1st Year')
-
-
-class SecondYearSubjectViewSet(generics.ListAPIView):
-    # permission_classes = [IsAuthenticated]
-    serializer_class = SubjectSerializer
-    queryset = Subject.objects.filter(year_level='2nd Year')
-
-
-class ThirdYearSubjectViewSet(generics.ListAPIView):
-    # permission_classes = [IsAuthenticated]
-    serializer_class = SubjectSerializer
-    queryset = Subject.objects.filter(year_level='3rd Year')
-
-
-class FourthYearSubjectViewSet(generics.ListAPIView):
-    # permission_classes = [IsAuthenticated]
-    serializer_class = SubjectSerializer
-    queryset = Subject.objects.filter(year_level='4th Year')
