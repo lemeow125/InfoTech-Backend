73df769a4a8e23f5dc3d44e748b191be6b59b6f8
keannu125 <noehbernasol0@gmail.com>
2023-04-22 17:07:14 +0800

    Made schedules field in students not required

--- infotech/students/serializers.py
+++ infotech/students/serializers.py
@@ -8,7 +8,7 @@ from subjects.models import Subject
 
 class StudentSerializer(serializers.HyperlinkedModelSerializer):
     schedules = serializers.SlugRelatedField(
-        queryset=Subject.objects.all(), many=True, slug_field='name', allow_null=True)
+        queryset=Subject.objects.all(), many=True, slug_field='name', allow_null=True, required=False)
 
     class Meta:
         model = Student
